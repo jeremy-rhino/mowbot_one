326a0d45b314586da20822b282b8f246ddb8534f
jeremy-rhino <88038495+jeremy-rhino@users.noreply.github.com>
2024-02-16 19:28:25 +1000

    update line 21

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -18,7 +18,7 @@ def generate_launch_description():
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('mowbot_one'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
-    robot_description_config = xacro.process_file(xacro_file).toxml()
+    robot_description_config = xacro.process_file(xacro_file)
     
     # Create a robot_state_publisher node
     params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
